e4c492cd86cd4929a95220129786204f58c4b9c9
Andreas Velounias <>
2021-03-09 20:33:15 +0000

    save path

--- app/lib/providers/great_places.dart
+++ app/lib/providers/great_places.dart
@@ -3,6 +3,8 @@ import 'dart:io';
 import 'package:app/helpers/db_helper.dart';
 import 'package:app/models/place.dart';
 import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:path/path.dart' as path;
 
 class GreatPlaces with ChangeNotifier {
   List<Place> _items = [];
@@ -29,13 +31,14 @@ class GreatPlaces with ChangeNotifier {
 
   Future<void> fetchAndSetPlaces() async {
     final dataList = await DBHelper.getData('user_places');
-    _items = dataList
-        .map((e) => Place(
-            id: e['id'],
-            image: File(e['image']),
-            title: e['title'],
-            location: null))
-        .toList();
+    final dbPath = await sql.getDatabasesPath();
+    _items = dataList.map((e) {
+      return Place(
+          id: e['id'],
+          image: File("$dbPath/${path.basename(e['image'])}"),
+          title: e['title'],
+          location: null);
+    }).toList();
     notifyListeners();
   }
 }
